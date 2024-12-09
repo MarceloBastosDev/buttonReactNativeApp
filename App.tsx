11475a6c26ab15c0bf5f46703834546f2be3590f
Marcelo <marcelo.bastos.silva007@gmail.com>
2024-12-09 16:20:49 -0300

    Adicionando função de Ligado e Desligado, alternando entre as imagens e textos

--- App.tsx
+++ App.tsx
@@ -1,15 +1,26 @@
-
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import symbolOff from './assets/off.jpg'
 import symbolOn from './assets/on.jpg'
 
 export default function App() {
+  const [isActive, setisActive] = useState(false)
   
+  function handleSymbol(){
+    setisActive((oldValue:boolean) =>{
+      return !oldValue
+    })
+    console.log(isActive)
+  }
+  const on = "Ligado!";
+  const off = "Desligado!";
   return (
     <View style={styles.container}>
-      <Image source={symbolOn}></Image>
-      <Image source={symbolOff}></Image>
+      <Text style={styles.statusText}>{isActive ? on : off}</Text>
+      <TouchableOpacity onPress={handleSymbol}>
+        <Image source={isActive ? symbolOn : symbolOff}></Image>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -22,4 +33,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  statusText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  }
 });
